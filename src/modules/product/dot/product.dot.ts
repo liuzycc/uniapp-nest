bc8f65df138aede93d5b6536842dffd64f9c55ce
liuzy8 <liuzy8@ziroom.com>
2024-05-14 18:25:01 +0800

    feat: mr

---
+++ src/modules/product/dot/product.dot.ts
@@ -0,0 +1,12 @@
+export class ProductDto {
+  readonly title: string;
+  readonly showTitle: string;
+  readonly subTitle: string;
+  readonly tags: string;
+  readonly thum: string;
+  readonly imgs: string;
+  readonly info: string;
+  readonly sort1: number;
+  readonly sort2: number;
+  readonly isDelete: number;
+}
---
+++ src/modules/shop/dot/shop.dot.ts
@@ -0,0 +1,7 @@
+export class ShopDto {
+  readonly userId: number;
+  readonly products: string;
+  readonly remark: string;
+  readonly status: number;
+  readonly isDelect: number;
+}
